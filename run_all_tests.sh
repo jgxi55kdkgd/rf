b32d6767a2a7daa20034ba799cff1477857d5f40
KSM <gsswhm@gmial.com>
2024-07-27 08:05:49 +0000

    eight

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,5 +1,16 @@
-PATH=/home/pwuser/.local/bin:$PATH
-export PATH
+#!/bin/bash
 
-rm -f CRM/Results/*
-xvfb-run robot -d CRM/Results CRM
+# Check if exactly one argument is passed
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <path-to-tests>"
+    exit 1
+fi
+
+# Assign the first argument to a variable
+TEST_PATH=$1
+
+# Remove previous test results
+rm -f ${TEST_PATH}/Results/*
+
+# Run Robot Framework tests with Xvfb
+xvfb-run robot -d ${TEST_PATH}/Results ${TEST_PATH}
--- start_xvfb.sh
+++ start_xvfb.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-PATH=/home/pwuser/.local/bin:${PATH}
-export PATH
-
 cd /workspaces/rf/
 # Start Xvfb in the background
 /usr/bin/Xvfb :99 -screen 0 1280x1024x24  &
-
